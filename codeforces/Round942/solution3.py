89f6856659170add7d5e6552ae4fd29cbe32fd13
dragonier23 <84836280+dragonier23@users.noreply.github.com>
2024-05-01 23:13:13 +0800

    d

--- codeforces/Round942/solution3.py
+++ codeforces/Round942/solution3.py
@@ -5,10 +5,13 @@ for i in range(testcases):
     a = list(map(int, input().split(" ")))
     a.sort()
     limit = len(a)
-    for i in range(1, len(a)):
-        if k < (a[i] * (i)) - sum(a[:i]):
-            limit = i
+    start, end = (0, len(a))
+    while True:
+        if k < (a[end] * (end)) - sum(a[:end]) and k > (a[start] * (start)) - sum(a[:start]) and end - start == -1:
+            limit = end
             break
+        middle = (start + end) // 2
+        
     k -= (a[limit-1] * (limit-1)) - sum(a[:(limit-1)])
     toAdd = k // limit
     remainder = k % limit
