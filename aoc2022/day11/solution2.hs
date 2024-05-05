c4bbe817c6c3039fffad16cbc08dfef24b555d2e
dragonier23 <84836280+dragonier23@users.noreply.github.com>
2024-05-05 23:10:04 +0800

    aoc 2022 day 11

--- aoc2022/day11/solution2.hs
+++ aoc2022/day11/solution2.hs
@@ -4,21 +4,22 @@ import qualified Data.Set as Set
 import Data.Char
 
 main = do
-    handle <- openFile "test1.txt" ReadMode
-    --handle <- openFile "problemstatement.txt" ReadMode
+    --handle <- openFile "test1.txt" ReadMode
+    handle <- openFile "problemstatement.txt" ReadMode
     contents <- hGetContents handle
     let parsedActions = parse contents
-        initState = parse'' . lines $ contents
-        postActions = solution initState . repeat' 10000 $ parsedActions
-        [x, y] = take 2 . reverse . sort $ (postActions !! 1)
+        modulo = parse''' parsedActions
+        initState = parse'' (lines contents) modulo
+        postActions = solution initState modulo (repeat' 10000 parsedActions)
+        [x, y] = take 2 . reverse . sort $ head (postActions !! 1)
         answer = x * y
-    print answer 
+    print answer
     hClose handle
 
-parse :: String -> [((String, Integer), Integer, Integer, Integer)]
+parse :: String -> [((String, Int), Int, Int, Int)]
 parse = parse' . lines
 
-parse' :: [String] -> [((String, Integer), Integer, Integer, Integer)]
+parse' :: [String] -> [((String, Int), Int, Int, Int)]
 parse' [] = []
 parse' (x:xs)
     | null x = parse' xs
@@ -28,38 +29,44 @@ parse' (_:_:a:b:c:d:xs) =
       value = if val == "old" then 2 else read val
   in ((opr, value), read . last . words $ b, read . last . words $ c, read . last . words $ d) : parse' xs
 
-parse'' :: [String] -> [[Integer]]
-parse'' [] = []
-parse'' (x:xs)
-  | null x = parse'' xs
-parse'' (_:x:_:_:_:_:xs) = initState : parse'' xs
+parse'' :: [String] -> [Int] -> [[[Int]]]
+parse'' [] _ = []
+parse'' (x:xs) modulo
+  | null x = parse'' xs modulo
+parse'' (_:x:_:_:_:_:xs) modulo = initStateMod : parse'' xs modulo
   where tmp = drop 2 . words $ x
         initState = map (\y -> if last y == ',' then read . init $ y else read y) tmp
+        initStateMod = map (\z -> map (\a -> z `mod` a) modulo) initState
 
-repeat' :: Integer -> [a] -> [a]
+parse''' :: [((String, Int), Int, Int, Int)] -> [Int]
+parse''' [] = []
+parse''' ((_, x, _, _):xs) = x:parse''' xs
+
+repeat' :: Int -> [a] -> [a]
 repeat' 0 _ = []
 repeat' x xs = xs ++ repeat' (x-1) xs
 
 -- our solution will do a fold, with acc being the current state, and moves feeding the current monkey's decision matrix
-solution :: [[Integer]] ->  [((String, Integer), Integer, Integer, Integer)] -> [[Integer]]
-solution states = foldl' (\([turn]:inspectionCount:xs) x ->
-  let items = xs !! fromIntegral turn -- extracting the current monkey's items
-      updatedInspectionCount = take (fromIntegral turn) inspectionCount ++ [(inspectionCount !! fromIntegral turn) + fromIntegral (length items)] ++ drop (fromIntegral turn + 1) inspectionCount
-      processItems = map (`processing` x) items -- deciding what to do with the items (of the form [(end val, next)])
-      finalState = foldl' (\acc (endVal, next) -> insert' next endVal acc) (take (fromIntegral turn) xs ++ [[]] ++ drop (fromIntegral turn + 1) xs) processItems
-  in [(turn + 1) `mod` fromIntegral (length states)] : updatedInspectionCount : finalState) ([0]:replicate (length states) 0:states)
+solution :: [[[Int]]] -> [Int] -> [((String, Int), Int, Int, Int)] -> [[[Int]]]
+solution states modulo = foldl (\([[turn]]:[inspectionCount]:xs) x ->
+  let items = xs !! turn -- extracting the current monkey's items
+      updatedInspectionCount = take turn inspectionCount ++ [(inspectionCount !! turn) + length items] ++ drop (turn + 1) inspectionCount
+      processItems = map (\y -> processing y modulo turn x) items -- deciding what to do with the items (of the form [(end val, next)])
+      finalState = foldl (\acc (endVal, next) -> insert' next endVal acc) (take turn xs ++ [[]] ++ drop (turn + 1) xs) processItems
+  in [[(turn + 1) `mod` length states]] : [updatedInspectionCount] : finalState) ([[0]]:[replicate (length states) 0]:states)
 
 -- we can figure out whose turn it is by looking at how many moves we have 
-insert' :: Integer -> Integer -> [[Integer]] -> [[Integer]]
-insert' monkey val xs = take (fromIntegral monkey) xs ++ [val : xs !! fromIntegral monkey] ++ drop (fromIntegral monkey + 1) xs
+insert' :: Int -> [Int] -> [[[Int]]] -> [[[Int]]]
+insert' monkey val xs = take monkey xs ++ [val : xs !! monkey] ++ drop (monkey + 1) xs
 
-processing :: Integer -> ((String, Integer), Integer, Integer, Integer) -> (Integer, Integer)
-processing item (("**", 2), test, pass, fail) =
-  let final = (item ^ 2)
-  in if (final `mod` test) == 0 then (final, pass) else (final, fail)
-processing item (("*", val), test, pass, fail) =
-  let final = (item * val)
-  in if (final `mod` test) == 0 then (final, pass) else (final, fail)
-processing item (("+", val), test, pass, fail) =
-  let final = (item + val)
-  in if (final `mod` test) == 0 then (final, pass) else (final, fail)
+-- processing needs to take the int, in the form of the mods, take the mods themselves, and the decision matrix, and the turn 
+processing :: [Int] -> [Int] -> Int -> ((String, Int), Int, Int, Int) -> ([Int], Int)
+processing item modulo turn (("**", 2), test, pass, fail) =
+  let final = zipWith (\x y -> (x ^ 2) `mod` y) item modulo
+  in if (final !! turn) == 0 then (final, pass) else (final, fail)
+processing item modulo turn (("*", val), test, pass, fail) =
+  let final = zipWith (\x y -> (x * val) `mod` y) item modulo
+  in if (final !! turn) == 0 then (final, pass) else (final, fail)
+processing item modulo turn (("+", val), test, pass, fail) =
+  let final = zipWith (\x y -> (x + val) `mod` y) item modulo
+  in if (final !! turn) == 0 then (final, pass) else (final, fail)
