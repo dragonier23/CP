b7265c8317580189e89198934bc5af0611148366
dragonier23 <84836280+dragonier23@users.noreply.github.com>
2024-05-12 15:30:07 +0800

    Round944 Q7

--- codeforces/Round944/solution7.py
+++ codeforces/Round944/solution7.py
@@ -1,28 +1,21 @@
 def swapable(i, j):
     return i >> 2 == j >> 2
 
-def search(mapping, curr):
-    if mapping[curr] == []:
-        return [curr]
-    ans = []
-    for x in mapping[curr]:
-        ans = ans + search(mapping, x)
-    return [curr] + ans
-
 for _ in range(int(input())):
     n = int(input())
     a = list(map(int, input().split(" ")))
-    potentialSwaps = [list(j for j in range(i+1, len(a)) if i != j and swapable(a[i], a[j]))for i in range(len(a))]
-    #this feels like some sort of graph traversal: lets do a bfs
-    swapSets = []
     passed = set()
-    for c in range(len(a)):
-        if not(c in passed):
-            tmp = set(search(potentialSwaps, c))
-            swapSets.append(tmp)
-            passed = passed.union(tmp)
-    #we now have the sets of swappables, and their indices
-    #for each element, we need to find the smallest in the set, and then 
+    swapSets = []
+    while len(passed) < n:
+        curr = set(i for i in range(n)).difference(passed).pop()
+        tmp = {curr}
+        toVisit = set(j for j in range(curr, len(a)) if swapable(a[curr], a[j]))
+        while toVisit:
+            curr = toVisit.pop()
+            tmp.add(curr)
+            toVisit.union(set(j for j in range(curr, len(a)) if swapable(a[curr], a[j] and not(j in tmp))))
+        passed = passed.union(tmp)
+        swapSets.append(tmp)
     swapSetElements = list(map(lambda x: sorted([a[e] for e in x]), swapSets))
     sortedArray = []
     for i in range(len(a)):
